a042a89416a0c75bdd3b99a074bd28024c9029fe
NuriyahD <nuriyahdavids2006@gmail.com>
2026-05-25 11:56:51 +0200

    Add files via upload

---
+++ Exercise 4/conditionals.js
@@ -0,0 +1,9 @@
+const score = 22
+const passingGrade = 60
+if ((score) >= (passingGrade) && (score) < 100){
+    console.log("Passed")
+}else if ((score) == 100){ 
+    console.log("Perfect score")
+}else {
+    console.log("Failed")
+}
